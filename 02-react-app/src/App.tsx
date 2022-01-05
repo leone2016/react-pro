35cf19c4dc509b8927719cefac73213e3e3888ef
ext_lpmedina <ext_lpmedina@falabella.cl>
2022-01-04 22:11:16 -0500

    react-router-dom V6

---
+++ 02-react-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import {Navigation} from "./routes/Navigation";
+
+function App() {
+    return (
+        <>
+           <Navigation />
+         </>
+    );
+}
+
+export default App;
